3689f3fc250031d55ee7dc0cd71947d34c5cc09d
ThanadolU <dolthanadolu@gmail.com>
2023-04-16 14:41:50 +0700

    🚩 add login system

--- src/routes/baseRouter.js
+++ src/routes/baseRouter.js
@@ -1,13 +1,16 @@
 const RestaurantRouter = require('./RestaurantRoute');
 const OrdersRouter = require('./OrderRoute')
 const MenusRouter = require('./MenuRoute')
+const UsersRouter = require('./UsersRoute')
 
 const router = require('express').Router();
 
+const redirectIfAuth = require('../middlewares/redirectIfAuth');
 
 router.use('/restaurants', RestaurantRouter);
 router.use('/orders', OrdersRouter);
-router.use('/menu', MenusRouter);
+router.use('/menus', MenusRouter);
+router.use('/users', redirectIfAuth, UsersRouter);
 
 
 module.exports = router;
