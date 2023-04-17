b02aac8be2c4ad8ae0f52a09b09e131cbfac4879
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-04-17 23:01:01 +0700

    ✏️ modify some payload

--- src/routes/OrderRoute.js
+++ src/routes/OrderRoute.js
@@ -3,7 +3,7 @@ const Orders = require('../models/Orders')
 
 const router = express.Router();
 
-router.get('/allOrders', async (req, res) => {
+router.get('/all', async (req, res) => {
     const orders = await Orders.getAllOrders(req, res);
     res.status(200).send({ "all_orders": orders });
 })
